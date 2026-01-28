58ee1760e63f86e6e6cba4dfeae7ec72744bbf03
Amandik94 <tulebaevamandik@gmail.com>
2026-01-28 21:32:09 +0500

    Начал сделать корзину

--- store/forms.py
+++ store/forms.py
@@ -8,6 +8,14 @@ class RegisterForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+        labels = {
+            'username': 'Логин',
+            'email': 'Email address',
+            'password': 'Пароль',
+        }
+        help_texts = {
+            'username': ('')
+        }
 
 class LoginForm(AuthenticationForm):
     pass
--- store/models.py
+++ store/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-
-
-
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     name = models.CharField(max_length=100) # название
